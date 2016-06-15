78ff9a80017a8159a5d86b5210d9534a527ace52
Display 6 <anthem@anthemdisplays.com>
2016-06-15 08:40:51 -0600

    Bootstrap.sh enhancements  - For production displays: try a ping before starting DHCP.    Only run DHCP if the display isn't online. This preserves    production inet settings without a reboot  - Added isyes() to bootstrap.sh  - Added install.log to the .gitignore.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,16 @@
 # Bootstrap a system controller installation
 # (c) Anthem Displays, 2016
 
+function isyes {
+    if [ "$1" = "Y" ] || [ "$1" = "y" ] || [ "$1" = "" ]; then
+        # Return 0 = true
+        return 0
+    else
+        # Return 1 = false
+        return 1
+    fi
+}
+
 if [ `whoami` == "root" ]; then
     echo "Running normally as root..."
 else
@@ -12,13 +22,23 @@ fi
 
 echo "-------------------------------------------"
 echo "TRYING TO GET ONLINE"
-ip route del default via 192.168.0.1
-ip route add default via 10.1.10.1
-dhclient -1 -pf /run/dhclient.p4p1.pid -lf /var/lib/dhcp/dhclient.p4p1.leases p4p1 &
-dhclient -1 -pf /run/dhclient.p1p1.pid -lf /var/lib/dhcp/dhclient.p1p1.leases p1p1 &
-
-for i in `seq 1 3`;
+for i in `seq 1 4`;
 do
+    echo "TRYING TO PING $i ..."
+
+    ping -c 1 8.8.8.8
+
+    if [ $? -eq 0 ]; then
+        echo "Ping was successful, we are online"
+        break
+    fi
+
+    echo Trying DHCP via 10.1.10.1
+    ip route del default via 192.168.0.1
+    ip route add default via 10.1.10.1
+    dhclient -1 -pf /run/dhclient.p4p1.pid -lf /var/lib/dhcp/dhclient.p4p1.leases p4p1 &
+    dhclient -1 -pf /run/dhclient.p1p1.pid -lf /var/lib/dhcp/dhclient.p1p1.leases p1p1 &
+
     sleep 1
     echo "Waiting 1..."
     sleep 1
@@ -31,16 +51,7 @@ do
     echo "Waiting 5..."
     sleep 1
 
-    echo "TRYING TO PING $i ..."
-
-    ping -c 1 8.8.8.8
-
-    if [ $? -eq 0 ]; then
-        echo "Ping was successful, we are online"
-        break
-    fi
-
-    if [ $i -eq 3 ]; then
+    if [ $i -eq 4 ]; then
         echo "-------------------------------------------"
         echo "ERROR: UNABLE TO GET ONLINE, PLEASE CONNECT AN ETHERNET CABLE"
         exit
