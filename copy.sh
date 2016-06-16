27cec8b5eee4e176d962eca81f0e1582b17aa5b1
Evan Brooks <dev@evanbrooks.net>
2016-06-16 16:02:09 -0600

    Bootstrap must be run as root.

--- copy.sh
+++ copy.sh
@@ -10,7 +10,7 @@
 cd /home/anthem/bootstrap
 git pull
 scp bootstrap.sh anthem@172.24.1.94:~/bootstrap/
-ssh anthem@172.24.1.94 bash ~/bootstrap/bootstrap.sh
+ssh anthem@172.24.1.94 bash sudo ~/bootstrap/bootstrap.sh
 #pause
 echo "An install log has been saved to the system controller as /home/anthem/install.log"
 read -p "Done. Press <Enter> to close the terminal."
